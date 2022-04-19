4bdc8d7a722629fd1adb227eb28f31adc930a6aa
Rondara Senbonzakura <99321513+Rondarka@users.noreply.github.com>
2022-04-19 15:50:39 +0300

    Add files via upload

---
+++ examAVT/examAVT/ClientsOrders.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace examAVT
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsOrders.xaml
+    /// </summary>
+    public partial class ClientsOrders : Window
+    {
+        SqlConnection con;
+        public ClientsOrders()
+        {
+            InitializeComponent();
+            con = new SqlConnection(@"Data source = DESKTOP-A87A485\SQLEXPRESS01; initial catalog = СалонКрасоты; integrated security = SSPI");
+            con.Open();
+            SqlCommand com = new SqlCommand("SELECT LastName+' '+ MiddleName+' '+FirstName,[Name], Price,[Date]" +
+                " from [Order] Left join Client on Client.ID = [Order].ClientID " +
+                "left join OrderService on OrderService.OrderID = [Order].ID " +
+                "left join[Service] on[Service].ID = OrderService.ServiceID", con);
+            DataTable table = new DataTable();
+            SqlDataReader r = com.ExecuteReader();
+            table.Load(r);
+            dtClients.ItemsSource = table.DefaultView;
+            con.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Orders newFonm = new Orders();
+            newFonm.Show();
+            this.Hide();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ examAVT/examAVT/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace examAVT
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection con;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            con = new SqlConnection(@"Data source = DESKTOP-A87A485\SQLEXPRESS01; initial catalog = СалонКрасоты; integrated security = SSPI");
+            con.Open();
+            SqlCommand com = new SqlCommand("Select * from [Employee] Where Login = '" + txtLogin.Text + "' and Password = '" + txtPass.Password + "'", con);
+            SqlDataReader r = com.ExecuteReader();
+            string userlogin = "";
+            string userpass = "";
+            try
+            {
+                r.Read();
+                userlogin = r["Login"].ToString();
+                userpass = r["Password"].ToString();
+            }
+            catch { };
+            if (txtLogin.Text == userlogin)
+            {
+                if(txtPass.Password == userpass)
+                {
+                    Orders order = new Orders();
+                    order.Show();
+                    Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Неверный пароль!");
+                }    
+            }
+            else
+            {
+                MessageBox.Show("Не удается найти пользователя!");
+            }
+        }
+    }
+}
